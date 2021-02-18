b725a87fa7f3eb66daf35aa40525ef6a07655976
john33fiao <john33fiao@gmail.com>
2021-02-18 05:53:40 +0000

    리스트

---
+++ official-tutorial/Ex07.py
@@ -0,0 +1,23 @@
+# 210118
+# 5. 자료 구조
+
+# 5.1. 리스트
+
+buks = []
+buks.append(35)
+print(buks, '----------')
+buks.extend({35, 23, 42})
+print(buks, '----------')
+buks.insert(3, '안녕하세요')
+print(buks, '----------')
+buks.remove(23)
+print(buks, '----------')
+print(buks.pop(1))
+print(buks, '----------')
+buks.clear
+print(buks, '----------')
+
+# https://docs.python.org/ko/3/tutorial/datastructures.html
+# 플립북 진짜 이걸로 코딩은 글러먹은듯
+# 진짜 모니터랑 씽패키보드 들고다니는게 나을 정도임
+# 어라 그거 그냥 아이패드잖아요
